ecfa32c8cb3f68e00024a1b2b49634e72c15e0cf
ramis2 <hundehumna@yahoo.com>
2026-01-09 23:49:09 -0500

    Create package.js

---
+++ package.js
@@ -0,0 +1,18 @@
+{
+  "name": "atlanta-taxi-dispatch-api",
+  "version": "1.0.0",
+  "description": "API for Atlanta Taxi Dispatch System",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js",
+    "dev": "nodemon server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "mongodb": "^6.3.0",
+    "cors": "^2.8.5"
+  },
+  "devDependencies": {
+    "nodemon": "^3.0.1"
+  }
+}
